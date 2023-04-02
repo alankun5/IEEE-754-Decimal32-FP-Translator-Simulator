441a37d89ef26151c0b40d3270fb9a7d3d4a4576
akn5 <104754302+alankun5@users.noreply.github.com>
2023-04-02 16:09:28 +0800

    #1: Add Project Template

---
+++ Decimal32FP-Sim/app/test/page.jsx
@@ -0,0 +1,63 @@
+"use client"
+import { TextField, Box, Button, Typography, Switch } from '@mui/material';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import AdbIcon from '@mui/icons-material/Adb';
+
+export default function TestPage() {
+    /* 
+    This is generally how a React useState is used. It takes two parameters:
+        value - the value of the state, in this case it is default to 0
+        setValue - is a function. we use setValue to change the value.
+
+    You can name this whatever you want, but it is generally good practice
+    to call it "variable" and "setVariable"
+    */
+    const [value, setValue] = useState(0)
+
+    /* 
+    useEffect takes two parameters, the first is a function and the
+    second is an array of values that it will watch for "changes".
+
+    Basically what useEffect does is it detects "changes" in the application
+    and it runs whatever is inside the function when it detects a change.
+    */
+    useEffect(() => {
+        console.log("Value has changed to: ", value);
+    }, [value])
+
+    const handleClick = () => {
+        setValue(value + 1);
+    }
+
+    const handleChange = () => {
+        console.log("Switch has been toggled");
+    }
+
+    /*
+    Place things inside the return statement that we want to see rendered
+    in the webpage.
+
+    The green tags here are called components. 
+
+    You can check out other Components you might take an interest in
+    from the Material UI Docs: https://mui.com/material-ui/getting-started/overview/
+    */
+    return(
+        <Box
+            sx={{
+                display: "flex-box",
+                backgroundColor: "white",
+                color: "black",
+            }}
+        >
+            <Typography variant='h5'>This is a Test Page</Typography>
+            <h5>normal H5</h5>
+            <p>Value: {value}</p>
+            <Button onClick={handleClick}>Click Me!</Button>
+            <TextField label="I am MUI TextField" />
+            <Switch onChange={handleChange} />
+            <AdbIcon />
+        </Box>
+    )
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # IEEE-754-Decimal32-FP-Translator-Simulator
 Simulation Project for CSARCH2
+
+---
+## Dev Notes
+
+**Running the Project**
+1. On your terminal, do > `cd Decimal32FP-Sim`
+2. then > `npm run dev`
+3. 
+
+**Installations Used**
+
+- NextJS: `npx create-next-app@latest`
+- MUI: `npm install @mui/icons-material @mui/material @emotion/styled @emotion/react`
+
+**Files & Directories**
+- `/app` : Contains pages & routing here
+    - `/test` : Test page routing for reference
+    - `layout.jsx` : Ignore this file but don't remove
+- `/public` : Place images, icons, and the like inside this folder
